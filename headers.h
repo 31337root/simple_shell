0ebbe98b4294208f94547503ccd4c0d6cc70c375
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-04-16 05:01:50 +0000

    add headers

---
+++ headers.h
@@ -0,0 +1,32 @@
+#ifndef _HEADERS_H_
+#define _HEADERS_H_
+
+/*   Libraries. */
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/*   Our functions. */
+
+char **space_organizer(char *string, const char *delim, int len, int *strcount)
+	;
+int one_finxer(char **arguments, char **path, int size0, int size1);
+int decision_taker(char **arguments, int size, char **_env);
+int executor(char **aop, const char *path, int size);
+void printenv(char **envp);
+void a_liberator(char **aop, int size);
+void path_finder(char **aop, int size, char **_env);
+
+/*   Generic functions. */
+
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *str);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+
+#endif /* _HEADERS_H_ */
